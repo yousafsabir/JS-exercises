77341562c2da6ddb5bf41f7fe6acbfade2bf615b
yousafsabir <yousafsabir430516@gmail.com>
2022-05-17 04:58:08 +0500

    hangmanV0.5(raw logic)

--- js8/hangman.js
+++ js8/hangman.js
@@ -94,20 +94,58 @@ let words = [
     "zero",
 ];
 
-let word1 = ["apple", "banana", "pear"];
+let main_div = document.querySelector(".inner-main");
+// index to later access it in Play()
+let index;
+let tries = 0;
 
-function main() {
+function create() {
+    // clearing previus word from main_div
+    main_div.innerHTML = "";
     // get random word from array
     // make it array
-    let word = words[getRnd(0, words.length - 1)].split("");
-    console.log(word);
-    // let main = document.querySelector(".main");
-    // for (let i = 0; i < 5; i++) {
-    //     let div = document.createElement("div");
-    //     div.classList.add("main-block");
-    //     main.appendChild(div);
-    // }
+    // let word = words[getRnd(0, words.length - 1)].split("");
+    let word = "abcc".split("");
+    // index = words.indexOf(word.join(""));
+    // tries = word.length + 3;
+    document.querySelector(".tries").innerHTML = `tries: ${tries}`;
+    // console.log("word:", word, "index:", index);
+    // create divs in dom
+    word.forEach((letter) => {
+        let div = document.createElement("div");
+        div.classList.add(`main-block`);
+        div.classList.add(`val-${letter}`);
+        div.innerHTML = letter;
+        main_div.appendChild(div);
+    });
+    console.log(document);
 }
+
+function play(id) {
+    if (tries === 0) {
+        return alert("you don't have any try left, play again");
+    } else {
+        let word = words[index].split("");
+        let match;
+        word.forEach((letter) => {
+            if (letter === id) {
+                match = true;
+            }
+        });
+        if (match) {
+            document.querySelector(`.val-${id}`).classList.add("visible");
+        } else if (!match) {
+            document.querySelector(`#${id}`).classList.add("fail");
+            document.querySelector(`#${id}`).disabled = true;
+            tries -= 1;
+            document.querySelector(".tries").innerHTML = `tries: ${tries}`;
+        }
+        let a = document.querySelectorAll(".visible");
+        console.log(a);
+    }
+}
+
+// utility functions
 
 function getRnd(min, max) {
     let step1 = max - min + 1;
@@ -115,5 +153,4 @@ function getRnd(min, max) {
     let result = Math.floor(step2) + min;
     return result;
 }
-
-main();
+let a = "b";
