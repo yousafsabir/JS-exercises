9064aebe83e806cb14f7cd7c846e6e46c7f1bcba
yousafsabir <myousafdev@gmail.com>
2024-05-15 21:11:42 +0500

    feat: solution for max sum of matrix elem

--- js28/index.js
+++ js28/index.js
@@ -1,34 +1,55 @@
 function maxSumOf(matrix) {
-  const sums = [];
+  let maxSum = -Infinity;
   for (let i = 0; i < matrix.length; i++) {
     for (let j = 0; j < matrix[i].length; j++) {
-      for (let k = i + 1; k <= matrix.length; k++) {
-        for (let l = j + 1; l <= matrix[i].length; l++) {
-          sums.push(
-            matrix
-              .slice(i, k)
-              .map((v) => v.slice(j, l))
-              .reduce(
-                (acc, curr) =>
-                  acc + curr.reduce((_acc, _curr) => _acc + _curr, 0),
-                0,
-              ),
-          );
+      for (let k = i; k < matrix.length; k++) {
+        for (let l = j; l < matrix[i].length; l++) {
+          let subMatrixSum = 0;
+          for (let m = i; m <= k; m++) {
+            for (let n = j; n <= l; n++) {
+              subMatrixSum += matrix[m][n];
+            }
+          }
+          if (subMatrixSum > maxSum) maxSum = subMatrixSum;
         }
       }
     }
   }
-  return sums.reduce((acc, curr) => (acc > curr ? acc : curr), -Infinity);
+  return maxSum;
+}
+
+function AhmadsMaxSumOf(matrix) {
+  const m = matrix.length,
+    n = matrix[0].length;
+  const sums = Array.from({ length: m + 1 }, (_) => Array(n).fill(0));
+  let r = 0;
+  for (let i = 0; i < m; i++) {
+    const si = sums[i + 1];
+    for (let j = 0; j < n; j++) {
+      si[j] = sums[i][j] + matrix[i][j];
+    }
+    for (let k = 0; k <= i; k++) {
+      let s = 0;
+      const sk = sums[k];
+      for (let j = 0; j < n; j++) {
+        s += si[j] - sk[j];
+        if (s < 0) s = 0;
+        if (r < s) r = s;
+      }
+    }
+  }
+  console.log(sums);
+  return r;
 }
 
 console.log(
-  maxSumOf([
-    [-1, -2, -3],
+  AhmadsMaxSumOf([
+    [1, 2, 3],
     [4, 5, 6],
-    [-7, -8, -9],
+    [7, 8, 9],
   ]),
 );
-console.log("Result: ", maxSumOf([[-1]]));
+// console.log("Result: ", maxSumOf([[1, 2, 3]]));
 
 // Find The Max Sum Of Contiguous-Submatrix In a Matrix - Ultimate Version
 // kata: https://www.codewars.com/kata/59ccf921a25c8c96460000ff/train/javascript
