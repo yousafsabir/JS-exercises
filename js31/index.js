e448f24024346cc3c1fd0df5d577e6cf94a21f50
yousafsabir <myousafdev@gmail.com>
2024-05-18 01:53:45 +0500

    feat: improved js31

--- js31/index.js
+++ js31/index.js
@@ -2,25 +2,19 @@
 // kata: https://www.codewars.com/kata/52742f58faf5485cae000b9a/train/javascript
 
 function formatDuration(seconds) {
-  if (!seconds) return "now";
-  const units = [
-    ["year", 31536000],
-    ["day", 86400],
-    ["hour", 3600],
-    ["minute", 60],
-    ["second", 1],
-  ];
-  let output = "";
-  for (let i = 0; i < units.length; i++) {
-    if (seconds < units[i][1]) continue;
-    let reminder = seconds % units[i][1];
-    if (output) {
-      output += !reminder ? " and " : ", ";
-    }
-    let unit = (seconds - reminder) / units[i][1];
-    output += unit + " " + units[i][0];
-    if (unit > 1) output += "s";
-    seconds -= unit * units[i][1];
-  }
-  return output;
+  return (
+    [
+      ["year", 31536000],
+      ["day", 86400],
+      ["hour", 3600],
+      ["minute", 60],
+      ["second", 1],
+    ].reduce((acc, curr) => {
+      if (seconds < curr[1]) return acc;
+      const value = ~~(seconds / curr[1]); // ~~ = Math.floor()
+      seconds -= value * curr[1];
+      if (acc) acc += seconds ? ", " : " and ";
+      return acc + `${value} ${curr[0]}${value === 1 ? "" : "s"}`;
+    }, "") || "now"
+  );
 }
