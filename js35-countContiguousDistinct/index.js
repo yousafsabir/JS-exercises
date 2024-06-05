b79f6a3c613832fb857e452085ec6ac46fd673dc
yousafsabir <myousafdev@gmail.com>
2024-06-05 15:32:06 +0500

    refactor: tried to refactor countContiguousDistinct

--- js35-countContiguousDistinct/index.js
+++ js35-countContiguousDistinct/index.js
@@ -1,14 +1,23 @@
 // countContiguousDistinct
 // kata: https://www.codewars.com/kata/5945f0c207693bc53100006b/train/javascript/
 
+console.time("duration:");
+
 function countContiguousDistinct(k, arr) {
   let res = [];
   for (let i = 0; i < arr.length - k + 1; i++) {
-    res.push([...new Set(arr.slice(i, i + k))].length);
+    let curr = [];
+    for (let j = i; j <= i + k - 1; j++) {
+      if (!curr.includes(arr[j])) {
+        curr.push(arr[j]);
+      }
+    }
+    res.push(curr.length);
   }
   return res;
 }
 
 console.log(countContiguousDistinct(4, [1, 2, 1, 3, 4, 2, 3]));
 
+console.timeEnd("duration:");
 // P.S: code works but timeouts on codewars
