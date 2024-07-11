db69165e87660d372bc44aad1b66071b4f3b021a
yousafsabir <myousafdev@gmail.com>
2024-07-11 14:08:02 +0500

    feat: js39 ltcde 1 Two sum, first solution

---
+++ js39-ltcde-1-two-sum/index.js
@@ -0,0 +1,18 @@
+// link: https://leetcode.com/problems/two-sum/
+
+// First Solution,
+// Time complexity: O(n^2)
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function (nums, target) {
+  for (let i = 0; i < nums.length - 1; i++) {
+    for (let j = i + 1; j < nums.length; j++) {
+      if (nums[i] + nums[j] === target) {
+        return [i, j];
+      }
+    }
+  }
+};
