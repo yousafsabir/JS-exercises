a1f8a1da784547aca322865c5e7865ff8a1e2de6
yousafsabir <myousafdev@gmail.com>
2024-07-11 14:20:13 +0500

    feat: js39 improved solution added

--- js39-ltcde-1-two-sum/index.js
+++ js39-ltcde-1-two-sum/index.js
@@ -1,13 +1,13 @@
 // link: https://leetcode.com/problems/two-sum/
 
-// First Solution,
+// First Solution (my own)
 // Time complexity: O(n^2)
 /**
  * @param {number[]} nums
  * @param {number} target
  * @return {number[]}
  */
-var twoSum = function (nums, target) {
+var twoSum1 = function (nums, target) {
   for (let i = 0; i < nums.length - 1; i++) {
     for (let j = i + 1; j < nums.length; j++) {
       if (nums[i] + nums[j] === target) {
@@ -15,4 +15,22 @@ var twoSum = function (nums, target) {
       }
     }
   }
+};
+
+// Second Solution (learned from the solutions)
+// Time complexity: O(n)
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum2 = function (nums, target) {
+  let numsObj = {};
+  for (let i = 0; i < nums.length; i++) {
+    if (target - nums[i] in numsObj) {
+      return [numsObj[target - nums[i]], i];
+    }
+    numsObj[nums[i]] = i;
+  }
+  return [];
 };
