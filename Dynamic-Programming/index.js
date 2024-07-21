e24e8229fdf36aa91df543aea39f54d7e4f1fe73
yousafsabir <myousafdev@gmail.com>
2024-07-21 13:28:12 +0500

    dynmc-prgm: feat: improved fibonacci solution with time complexity of O(n)

--- Dynamic-Programming/index.js
+++ Dynamic-Programming/index.js
@@ -1,9 +1,23 @@
 // 1:
 // Problem: return the nth fibonacci number
 
-function nthFib(n) {
+function nthFib1(n) {
   if (n <= 2) return 1;
-  return nthFib(n - 1) + nthFib(n - 2);
+  return nthFib1(n - 1) + nthFib1(n - 2);
 }
 
-console.log(nthFib(7));
+function nthFib2(n) {
+  let sequence = Array(n);
+
+  for (let i = 0; i < n; i++) {
+    if (i <= 1) {
+      sequence[i] = 1;
+      continue;
+    }
+    sequence[i] = sequence[i - 1] + sequence[i - 2];
+  }
+
+  return sequence[n - 1];
+}
+
+console.log(nthFib2(50));
