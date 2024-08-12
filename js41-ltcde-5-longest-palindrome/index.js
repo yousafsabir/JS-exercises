76be104f16e6f6fc277cd0c5b68ba658d45eb065
yousafsabir <myousafdev@gmail.com>
2024-08-12 17:01:52 +0500

    feat: ltcde 5: finally devised the accepted solution

--- js41-ltcde-5-longest-palindrome/index.js
+++ js41-ltcde-5-longest-palindrome/index.js
@@ -1,29 +1,5 @@
 // link: https://leetcode.com/problems/longest-palindromic-substring/
 
-// /**
-//  * @param {string} s
-//  * @return {string}
-//  */
-// function longestPalindrome(s) {
-//   if (s.length === 1) return s;
-
-//   let longestPalindromeYet = "";
-
-//   for (let i = 0; i < s.length - 1; i++) {
-//     for (let j = i + 1; j < s.length + 1; j++) {
-//       let substr = s.substring(i, j);
-//       if (
-//         substr === reversedStr(substr) &&
-//         substr.length > longestPalindromeYet.length
-//       ) {
-//         longestPalindromeYet = substr;
-//       }
-//     }
-//   }
-
-//   return longestPalindromeYet;
-// }
-
 /**
  * @param {string} s
  * @return {string}
@@ -31,35 +7,32 @@
 function longestPalindrome(s) {
   if (s === reversedStr(s)) return s;
 
-  let longestPalindromeYet = s[0];
+  let longest = "";
 
-  let i = 0;
-
-  while (i < s.length) {
-    let p = ""; // palindrome test substring
+  for (let i = 0; i < s.length; i++) {
+    let p = s[i]; // palindrome test substring
     let x = i ? i - 1 : i;
     let y = i + 1;
-    // check for odd char count palindrome
-    while (x >= 0 && y < s.length) {
-      if (s.substring(x, y + 1) === reversedStr(s.substring(x, y + 1))) {
-        p = s.substring(x, y + 1);
-      } else if (!p && i !== 0 && x === i - 1) {
-        // one time execution to set x for even char palindrome
-        x++;
-        continue;
-      } else break;
 
-      x -= 1;
-      y += 1;
+    while (x >= 0 && y < s.length) {
+      let substr = s.slice(i ? x + 1 : x, y);
+      if (substr + s[y] === reversedStr(substr + s[y])) {
+        p = substr + s[y];
+        y++;
+      } else if (
+        i &&
+        s[x] + substr + s[y] === reversedStr(s[x] + substr + s[y])
+      ) {
+        p = s[x] + substr + s[y];
+        x--;
+        y++;
+      } else break;
     }
 
-    // check if p is a palindrome
-    if (p.length && p.length > longestPalindromeYet.length)
-      longestPalindromeYet = p;
-    i++; // outer index increment
+    if (p.length > longest.length) longest = p;
   }
 
-  return longestPalindromeYet;
+  return longest.length;
 }
 
 /**
@@ -75,3 +48,4 @@ console.log(
     "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaabcaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa",
   ),
 );
+console.log(longestPalindrome("bacabab"));
