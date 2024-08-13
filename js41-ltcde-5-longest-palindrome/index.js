39f1d34556615a2df7b8ced98214827f0cd28e1b
yousafsabir <myousafdev@gmail.com>
2024-08-13 22:57:58 +0500

    feat: ltcde 5: solution further improved

--- js41-ltcde-5-longest-palindrome/index.js
+++ js41-ltcde-5-longest-palindrome/index.js
@@ -5,47 +5,43 @@
  * @return {string}
  */
 function longestPalindrome(s) {
-  if (s === reversedStr(s)) return s;
-
   let longest = "";
 
   for (let i = 0; i < s.length; i++) {
-    let p = s[i]; // palindrome test substring
-    let x = i ? i - 1 : i;
-    let y = i + 1;
+    let even = expandAround(i, i, s);
+    let odd = expandAround(i, i + 1, s);
+    let p = even.length > odd.length ? even : odd;
 
-    while (x >= 0 && y < s.length) {
-      let substr = s.slice(i ? x + 1 : x, y);
-      if (substr + s[y] === reversedStr(substr + s[y])) {
-        p = substr + s[y];
-        y++;
-      } else if (
-        i &&
-        s[x] + substr + s[y] === reversedStr(s[x] + substr + s[y])
-      ) {
-        p = s[x] + substr + s[y];
-        x--;
-        y++;
-      } else break;
+    if (p.length > longest.length) {
+      longest = p;
     }
-
-    if (p.length > longest.length) longest = p;
   }
-
   return longest.length;
 }
 
 /**
+ * @param {number} x
+ * @param {number} y
  * @param {string} s
  * @return {string}
  */
-function reversedStr(s) {
-  return s.split("").reduce((acc, curr) => curr + acc, "");
+function expandAround(x, y, s) {
+  while (x >= 0 && y < s.length && s[x] === s[y]) {
+    x--;
+    y++;
+  }
+  return s.slice(x + 1, y);
 }
 
+console.log(longestPalindrome("a"));
+console.log(longestPalindrome("abc"));
+console.log(longestPalindrome("baabc"));
+console.log(longestPalindrome("babc"));
+console.log(longestPalindrome("ccc"));
+// console.log(longestPalindrome("abcdbbd"));
+// console.log(longestPalindrome("bacabab"));
 console.log(
   longestPalindrome(
     "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaabcaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa",
   ),
 );
-console.log(longestPalindrome("bacabab"));
