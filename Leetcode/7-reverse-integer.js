0a6e10e1f344425a01980be7d36bd79783b78411
yousafsabir <myousafdev@gmail.com>
2024-08-19 13:26:06 +0500

    feat: ltcde 7: solution optimized

--- Leetcode/7-reverse-integer.js
+++ Leetcode/7-reverse-integer.js
@@ -3,17 +3,15 @@
  * @return {number}
  */
 function reverse(x) {
-  let int = x.toString().split("");
+  x = x.toString().split("");
   let sign = false;
-  if (int[0] === "-") {
-    int.unshift();
+  if (x[0] === "-") {
+    x.unshift();
     sign = true;
   }
-  int = parseInt(int.reverse().join(""));
-  if (sign) int *= -1;
+  x = parseInt(x.reverse().join(""));
+  if (sign) x *= -1;
 
-  if (int < -2147483648 || int > 2147483647) return 0;
-  return int;
+  if (x < -2147483648 || x > 2147483647) return 0;
+  return x;
 }
--2147483648;
-2147483647;
